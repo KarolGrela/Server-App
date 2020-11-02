48866bd2693ac1a4839974ddb2081e2af1e7b645
karol.grela <karol.grela@vp.pl>
2020-11-02 16:13:03 +0100

    Add project files.

---
+++ Server App/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// C# Socket Programming - Multiple Clients
+
+namespace Server_App
+{
+    class Program
+    {
+        private static byte[] buffer = new byte[1024];
+        private static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> clientSockets = new List<Socket>();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private void SetupServer() 
+        {
+            Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6001));
+            serverSocket.Listen(5);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = serverSocket.EndAccept(AR);
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(buffer, dataBuf, received);
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Text received: " + text);
+
+        }
+
+        public static void ExecuteServer()
+        {
+            // Establish the local endpoint  
+            // for the socket. Dns.GetHostName 
+            // returns the name of the host  
+            // running the application. 
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP Socket using  
+            // Socket Class Costructor 
+            Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                // Using Bind() method we associate a 
+                // network address to the Server Socket 
+                // All client that will connect to this  
+                // Server Socket must know this network 
+                // Address 
+                listener.Bind(localEndPoint);
+
+                // Using Listen() method we create  
+                // the Client list that will want 
+                // to connect to Server 
+                listener.Listen(10);
+
+                while (true)
+                {
+
+                    Console.WriteLine("Waiting connection ... ");
+
+                    // Suspend while waiting for 
+                    // incoming connection Using  
+                    // Accept() method the server  
+                    // will accept connection of client 
+                    Socket clientSocket = listener.Accept();
+
+                    // Data buffer 
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+
+                        int numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes,
+                                                   0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    // Send a message to Client  
+                    // using Send() method 
+                    clientSocket.Send(message);
+
+                    // Close client Socket using the 
+                    // Close() method. After closing, 
+                    // we can use the closed Socket  
+                    // for a new Client Connection 
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }
+
+
+}
